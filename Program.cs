8857341327aec952cee0a2c4fcd96a51bad5a260
Daniela Duarte <danielatavaresd@gmail.com>
2024-02-07 23:39:51 +0000

    JogoDaVelha

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[,] matriz = new string[3,3];
+
+        //Alimentando a matriz 
+
+        for (int i = 0; i < matriz.GetLength(0); i++)
+        {
+            for (int j = 0; j < matriz.GetLength(1); j++)
+            {
+                matriz[i, j] = "0";
+            }
+        }
+
+        //Imprimir a matriz 
+        for (int i = 0; i < matriz.GetLength(0); i++)
+        {
+            for (int j = 0; j < matriz.GetLength(1); j++)
+            {
+                Console.WriteLine($" [{matriz[i, j]}] ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
